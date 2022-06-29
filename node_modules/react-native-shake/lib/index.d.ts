2e3b81cd7d7e1aabcbc256461c66651d7b50954a
Gustavo Rodrigues Martins <gustavo.martins145@etec.sp.gov.br>
2022-06-29 18:38:11 -0300

    APP com React Native pela DIO

---
+++ node_modules/react-native-shake/lib/index.d.ts
@@ -0,0 +1,6 @@
+declare const _default: {
+    addListener: (callback: () => void | undefined) => import("react-native").EmitterSubscription;
+    removeAllListeners: () => void;
+    removeCurrentListener: () => void;
+};
+export default _default;
